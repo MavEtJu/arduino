e1482612424627f17af82b9bfec4e7fa1c0351f4
Edwin Groothuis <wireshark@mavetju.org>
2015-02-18 12:39:00 +1100

    Also use the returned values here

--- pixelsim/StringEncodeMain.cpp
+++ pixelsim/StringEncodeMain.cpp
@@ -9,30 +9,35 @@ void
 test(void)
 {
 	StringEncode *enc = new StringEncode();
-	int width, height;
-	int encodedStringPlainLength = 16;
+	int width;
+	uint16_t encodedStringPlainLength;
+	uint16_t decodedStringPlainLength;
 	char encodedStringPlain[100];
 	char decodedStringPlain[300];
 	char s[300];
 
 	width = 16;
-	height = 9;
+//	encodedStringPlainLength = 16;
 //	memcpy(encodedStringPlain,
 //	    "\x20\x40\x90\x90\xbf\xd0\xef\x70\x7f\xe0\x3f\xc0\x20\x40\x60\x60",
 //	    width * height / 8);
+	encodedStringPlainLength = 18;
 	memcpy(encodedStringPlain,
 	    "\x22\x00\x14\x00\x3e\x00\x6b\x00\xff\x80\xbe\x80\xbe\x80\x14\x00\x36\x00",
-	    width * height / 8);
+	    encodedStringPlainLength);
 	enc->DecodePlain(encodedStringPlain, decodedStringPlain,
-		encodedStringPlainLength);
-	for (int i = 0; i < height; i++) {
+		encodedStringPlainLength, &decodedStringPlainLength);
+	printf("encodedStringPlainLength:%d decodedStringPlainLength:%d\n",
+		encodedStringPlainLength, decodedStringPlainLength);
+	for (int i = 0; i < decodedStringPlainLength / width; i++) {
 		char ss[20];
 		strncpy(ss, decodedStringPlain + i * width, width);
 		ss[width] = '\0';
 		printf("%s\n", ss);
 	}
 
-	enc->EncodePlain(decodedStringPlain, s, strlen(decodedStringPlain));
+	enc->EncodePlain(decodedStringPlain, s, decodedStringPlainLength,
+	    &encodedStringPlainLength);
 	printf("memcmp(original, encoded(decoded), %d) = %d\n",
 	    encodedStringPlainLength,
 	    memcmp(encodedStringPlain, s, encodedStringPlainLength));
@@ -41,8 +46,8 @@ test(void)
 	enc->hexdump(s, encodedStringPlainLength);
 
 	enc->DecodePlain(s, decodedStringPlain,
-		encodedStringPlainLength);
-	for (int i = 0; i < height; i++) {
+		encodedStringPlainLength, &decodedStringPlainLength);
+	for (int i = 0; i < decodedStringPlainLength / width; i++) {
 		char ss[20];
 		strncpy(ss, decodedStringPlain + i * width, width);
 		ss[width] = '\0';
@@ -57,6 +62,7 @@ process(char *f)
 	FILE *fin;
 	char s[10000], *ps, out[1000], line[100];
 	int width, height;
+	uint16_t declen, enclen;
 	StringEncode *enc = new StringEncode();
 
 	if ((fin = fopen(f, "r")) == NULL)
@@ -91,8 +97,9 @@ process(char *f)
 	height = strlen(s) / width;
 	printf("width: %d\nheight: %d\n", width, height);
 
-	enc->EncodePlain(s, out, strlen(s));
-	enc->hexdump(out, strlen(s) / 8);
+	declen = strlen(s);
+	enc->EncodePlain(s, out, declen, &enclen);
+	enc->hexdump(out, enclen);
 }
 
 
