f7bebec65df8421bb8f65e45c80217510eea0834
Edwin Groothuis <wireshark@mavetju.org>
2015-02-18 12:36:42 +1100

    Properly initialize the returned value. Also when no output file has been given, give an estimation of the amount of data returned.

--- toiletroll_draw/StringEncode.cpp
+++ toiletroll_draw/StringEncode.cpp
@@ -11,12 +11,13 @@ StringEncode::StringEncode(void)
     // Nothing
 }
 
-void
-StringEncode::EncodePlain(const char *in, char *out, uint16_t plainLen)
+uint16_t
+StringEncode::EncodePlain(const char *in, char *out, uint16_t plainLen, uint16_t *encodedLen)
 {
     uint16_t c;
     uint16_t bits[8];
 
+    *encodedLen = 0;
     // printf("plainLen:%d\n", plainLen);
     for (c = 0; c < plainLen; c += 8) {
 	// printf("c:%d\n", c);
@@ -28,29 +29,39 @@ StringEncode::EncodePlain(const char *in, char *out, uint16_t plainLen)
 	bits[2] = in[c + 5] != ' ';
 	bits[1] = in[c + 6] != ' ';
 	bits[0] = in[c + 7] != ' ';
-	out[c / 8] = (bits[7] << 7) + (bits[6] << 6) + (bits[5] << 5) +
-		     (bits[4] << 4) + (bits[3] << 3) + (bits[2] << 2) +
-		     (bits[1] << 1) + bits[0];
+	if (out != NULL)
+	    out[c / 8] = (bits[7] << 7) + (bits[6] << 6) + (bits[5] << 5) +
+			 (bits[4] << 4) + (bits[3] << 3) + (bits[2] << 2) +
+			 (bits[1] << 1) + bits[0];
 	// printf("x:%x\n", out[c/8]);
+	(*encodedLen)++;
     }
+    return *encodedLen;
 }
 
-void
-StringEncode::DecodePlain(const char *in, char *out, uint16_t encodedLen)
+uint16_t
+StringEncode::DecodePlain(const char *in, char *out, uint16_t encodedLen, uint16_t *decodedLen)
 {
     uint16_t c;
     unsigned char ch;
 
-    out[0] = 0;
+    *decodedLen = 0;
+
+    if (out != NULL)
+	out[0] = 0;
     for (c = 0; c < encodedLen; c++) {
 	ch = in[c];
 	for (int8_t i = 7; i >= 0; i--) {
-	    if ((ch >> i) % 2 != 0)
-		strcat(out, "X");
-	    else
-		strcat(out, " ");
+	    if (out != NULL) {
+		if ((ch >> i) % 2 != 0)
+		    strcat(out, "X");
+		else
+		    strcat(out, " ");
+	    }
+	    (*decodedLen)++;
 	}
     }
+    return *decodedLen;
 }
 
 void
