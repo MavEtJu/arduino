45825350f6636210062115707a915f686eb754ee
Edwin Groothuis <wireshark@mavetju.org>
2015-02-26 08:26:21 +1100

    Change bitmaps into string encoded data.

--- toiletroll_draw/toiletroll_draw.ino
+++ toiletroll_draw/toiletroll_draw.ino
@@ -583,10 +583,9 @@ LED_spaceinvaders1::animation(void)
 class LED_mario1 : public LED_Slideshow {
 public:
     LED_mario1(void);
-    uint8_t COLOUR_SKIN;
 };
 
-LED_mario1::LED_mario1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
+LED_mario1::LED_mario1(void) : LED_Slideshow(&led, VIEW_WIDTH, VIEW_HEIGHT)
 {
     /*
      * . black
@@ -602,7 +601,11 @@ LED_mario1::LED_mario1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
      * B brown
      */
     set_imgs(2);
+
+    width[0] = 13;
+    bits[0] = 624;
     imgs[0] = PSTR(
+	/*
         "     RRRRRR     "
         "    RRRRRRRRRR  "
         "    rrrSSS.S    "
@@ -619,13 +622,18 @@ LED_mario1::LED_mario1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "    BBB  BBB    "
         "   ....  ....   "
         "  .....  .....  "
+	*/
+	"\x07\x20\x2e\x42\x52\x53\x72\x79\x00\xb6\x6d\x03\x00\x60\xdb\xb6\x6d\x03\xd0\x96\x64\x08\x00\x65\x49\x32\x24\x81\xb2\x24\xc9\x90\x44\x4b\x92\x4c\x12\x00\x20\x49\x92\x04\x80\x4d\xdb\x04\x00\xd8\xa6\x4d\xdb\x80\x6d\x93\xa4\x6d\x03\x39\x59\xb2\x46\x82\xa4\x24\x49\x24\x41\x4a\x92\x24\x91\x00\x24\x01\x92\x00\x20\x49\x80\x24\x01\x92\x24\x40\x92\x04"
     );
     /*
      * R red
      * W white
      * B black
      */
+    width[1] = 16;
+    bits[1] = 512;
     imgs[1] = PSTR(
+	/*
         "     ......     "
         "   ..RRRRWW..   "
         "  .WWRRRRWWWW.  "
@@ -642,7 +650,10 @@ LED_mario1::LED_mario1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "  .WWWWWWWWWW.  "
         "   ..WWWWWW..   "
         "     ......     "
+	*/
+	"\x04\x20\x2e\x52\x57\x00\x54\x15\x00\x40\xa9\x7e\x01\xd0\xab\xfe\x07\xf4\xaa\xfa\x1f\xf4\xfe\xeb\x1f\xa9\xff\xaf\x6a\xa9\xff\xaf\x6f\xad\xff\xef\x7f\xbd\xfe\xeb\x7f\xbd\xaa\xaa\x6f\xad\x55\x55\x6a\x54\xdf\xf7\x15\xd0\xdf\xf7\x07\xd0\xff\xff\x07\x40\xfd\x7f\x01\x00\x54\x15\x00"
     );
+
     add_colourmap('S', COLOUR_MAX + 1, 208 >> 4, 189 >> 4, 156 >> 4);
 }
 
@@ -652,7 +663,7 @@ public:
     LED_galaga1(void);
 };
 
-LED_galaga1::LED_galaga1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
+LED_galaga1::LED_galaga1(void) : LED_Slideshow(&led, VIEW_WIDTH, VIEW_HEIGHT)
 {
     /*
      * R red
@@ -663,7 +674,10 @@ LED_galaga1::LED_galaga1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
      *
      */
     set_imgs(3);
+    width[0] = 15;
+    bits[0] = 480;
     imgs[0] = PSTR(
+	/*
         "       W        "
         "       W        "
         "       W        "
@@ -680,12 +694,17 @@ LED_galaga1::LED_galaga1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "WWW RRWWWRR WWW "
         "WW  RR W RR  WW "
         "W      W      W "
+	*/
+	"\x04\x20\x42\x52\x57\x00\xc0\x00\x00\x00\x30\x00\x00\x00\x0c\x00\x00\xc0\x0f\x00\x00\xf0\x03\x00\x00\xfc\x00\x00\x08\x3f\x08\x00\xf2\x3f\x02\xc2\xbd\xdf\xa0\xd0\xab\x1f\x38\xfc\xee\x0f\xcf\xff\xff\xcf\xff\xfb\xfb\xff\x8f\xfe\xca\xff\xa0\x8c\xc2\x0f\x00\x03\xc0"
     );
 
     /*
      * From http://fc03.deviantart.net/fs50/f/2009/271/8/f/Galaga_Sprites_by_mammaDX.png
      */
+    width[1] = 13;
+    bits[1] = 260;
     imgs[1] = PSTR(
+	/*
         "                "
         "                "
         " BB   y   BB    "
@@ -702,9 +721,14 @@ LED_galaga1::LED_galaga1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "                "
         "                "
         "                "
+	*/
+	"\x04\x20\x42\x52\x79\x14\x30\x50\x40\xed\x5e\x00\xa4\x6b\x00\xc0\xff\x00\x00\xfd\x01\x40\xa5\x56\x40\x85\x4a\x45\x15\x3f\x55\x15\xa8\x50\x55\x80\x40\x05"
     );
 
+    width[2] = 15;
+    bits[2] = 480;
     imgs[2] = PSTR(
+	/*
         "      G G       "
         "      G G       "
         "   GGRRGRRGG    "
@@ -721,6 +745,8 @@ LED_galaga1::LED_galaga1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "GRRG       GRRG "
         "GGGG       GGGG "
         " GG         GG  "
+	*/
+	"\x04\x20\x47\x52\x79\x00\x10\x01\x00\x00\x44\x00\x00\x94\xa6\x05\x00\xa4\x69\x00\x00\x54\x05\x00\x40\xdf\x07\x00\xd5\xff\x15\x54\xf5\x7f\x15\x54\xfd\x5f\x01\x59\x88\x94\x51\x06\x22\x64\x65\x01\x00\x65\x69\x00\x40\x5a\x1a\x00\x90\x56\x05\x00\x54\x51\x00\x00\x14"
     );
 }
 
@@ -877,14 +903,16 @@ LED_minecraft1::create_colourmap(void)
     add_colourmap('s', 29, 255 >> 4, 210 >> 4,   4 >> 4);
 }
 
-LED_minecraft1::LED_minecraft1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
+LED_minecraft1::LED_minecraft1(void) : LED_Slideshow(&led, VIEW_WIDTH, VIEW_HEIGHT)
 {
     create_colourmap();
     set_imgs(6);
 
-    
     /* From http://fc04.deviantart.net/fs71/f/2013/162/9/5/minecraft_sword_patterns_by_sarrel-d68p8xf.png */
+    bits[0] = 1024;
+    width[0] = 16;
     imgs[0] = PSTR(
+	/*
         "             888"
         "            8458"
         "           84548"
@@ -901,10 +929,15 @@ LED_minecraft1::LED_minecraft1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "9913            "
         "969             "
         "999             "
+	*/
+	"\x0b\x20\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x00\x00\x00\x00\x00\x00\x90\x99\x00\x00\x00\x00\x00\x00\x59\x96\x00\x00\x00\x00\x00\x90\x65\x95\x00\x00\x00\x00\x00\x59\x56\x09\x00\x00\x00\x00\x90\x65\x95\x00\x00\x00\x00\x00\x59\x56\x09\x00\x00\xaa\x00\x90\x65\x95\x00\x00\x00\x8a\x0a\x59\x56\x09\x00\x00\x00\xa0\xa7\x65\x95\x00\x00\x00\x00\xa0\xa7\x56\x09\x00\x00\x00\x00\x00\x7a\xaa\x00\x00\x00\x00\x00\x30\xa2\x77\x0a\x00\x00\x00\x00\x13\x04\xaa\xa8\x00\x00\x00\xaa\x42\x00\x00\x00\x00\x00\x00\x7a\x0a\x00\x00\x00\x00\x00\x00\xaa\x0a\x00\x00\x00\x00\x00\x00"
         );
 
     /* http://fc04.deviantart.net/fs71/f/2013/162/9/8/minecraft_pickaxe_patterns_by_sarrel-d68p8h7.png */
+    bits[1] = 676;
+    width[1] = 13;
     imgs[1] = PSTR(
+	/*
         "                "
         "                "
         "     88888      "
@@ -921,9 +954,14 @@ LED_minecraft1::LED_minecraft1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         " 203            "
         " 13             "
         "                "
+	*/
+	"\x09\x20\x30\x31\x32\x33\x34\x35\x36\x38\x00\x00\x88\x88\x08\x00\x00\x00\x58\x76\x67\x38\x02\x00\x00\x88\x88\x77\x41\x00\x00\x00\x00\x30\x76\x08\x00\x00\x00\x30\x42\x67\x08\x00\x00\x30\x41\x80\x87\x00\x00\x30\x42\x00\x78\x08\x00\x30\x41\x00\x80\x86\x00\x30\x42\x00\x00\x58\x08\x30\x41\x00\x00\x00\x08\x30\x42\x00\x00\x00\x00\x30\x41\x00\x00\x00\x00\x00\x42\x00\x00\x00\x00\x00\x00"
     );
     /* http://fc06.deviantart.net/fs70/f/2013/162/8/5/minecraft_axe_patterns_by_sarrel-d68p7v3.png */
+    bits[2] = 672;
+    width[2] = 12;
     imgs[2] = PSTR(
+	/*
         "                "
         "         88     "
         "        8448    "
@@ -940,10 +978,15 @@ LED_minecraft1::LED_minecraft1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "  203           "
         "  13            "
         "                "
+	*/
+	"\x0a\x20\x30\x31\x32\x33\x34\x35\x36\x37\x38\x00\x00\x00\x90\x09\x00\x00\x00\x00\x59\x95\x00\x00\x00\x90\x75\x96\x00\x00\x00\x59\x77\x37\x02\x00\x00\x59\x76\x78\x04\x00\x00\x90\x39\x87\x97\x00\x00\x00\x13\x74\x97\x00\x00\x30\x42\x90\x09\x00\x00\x13\x04\x00\x00\x00\x30\x42\x00\x00\x00\x00\x13\x04\x00\x00\x00\x30\x42\x00\x00\x00\x00\x13\x04\x00\x00\x00\x00\x42\x00\x00\x00\x00\x00"
         );
 
     /* http://fc07.deviantart.net/fs70/f/2013/162/e/1/minecraft_shovel_patterns_by_sarrel-d68p8pl.png */
+    bits[3] = 676;
+    width[3] = 13;
     imgs[3] = PSTR(
+	/*
         "                "
         "                "
         "          888   "
@@ -960,10 +1003,16 @@ LED_minecraft1::LED_minecraft1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         " 213            "
         "  23            "
         "                "
+	*/
+	"\x09\x20\x30\x31\x32\x33\x34\x35\x36\x38\x00\x00\x00\x00\x80\x88\x00\x00\x00\x00\x80\x55\x87\x00\x00\x00\x80\x65\x57\x08\x00\x00\x80\x65\x67\x85\x00\x00\x00\x30\x67\x85\x00\x00\x00\x30\x41\x85\x00\x00\x00\x30\x41\x80\x00\x00\x00\x30\x42\x00\x00\x00\x00\x30\x41\x00\x00\x00\x00\x30\x42\x00\x00\x00\x00\x34\x41\x00\x00\x00\x00\x30\x42\x00\x00\x00\x00\x00\x30\x04\x00\x00\x00\x00\x00"
         );
 
     /* Water and lava bucket */
+
+    bits[4] = 780;
+    width[4] = 12;
     imgs[4] = PSTR(
+	/*
         "                "
         "                "
         "     aaabbb     "
@@ -980,9 +1029,14 @@ LED_minecraft1::LED_minecraft1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "    bdffddda    "
         "     aaaaaa     "
         "                "
+	*/
+	"\x11\x20\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x00\x80\x10\x82\x10\x02\x00\x00\x42\x70\xef\xbd\x07\x84\x00\xa1\xb5\xa5\x94\x52\x09\xc2\x20\x02\x5b\x4b\x29\x95\x52\x08\xc2\x84\xb0\x94\x52\x21\x8c\x20\x8c\x31\x21\x84\x90\xd2\x08\xc2\x98\x83\x4a\x29\x89\x8c\x00\x84\x39\xa6\x94\x92\x46\x00\x40\x18\x73\x4c\x21\x64\x04\x00\x84\x31\xc7\x14\x92\x46\x00\x00\x08\x63\x4c\x21\x24\x00\x00\x80\x20\xc6\x10\x42\x02\x00\x00\x80\x10\x42\x08\x01\x00\x00"
         );
 
+    bits[5] = 624;
+    width[5] = 12;
     imgs[5] = PSTR(
+	/*
         "                "
         "                "
         "     aaabbb     "
@@ -999,6 +1053,8 @@ LED_minecraft1::LED_minecraft1(void) : LED_Slideshow(&led, VIEW_HEIGHT)
         "    bdffddda    "
         "     aaaaaa     "
         "                "
+	*/
+	"\x0b\x20\x61\x62\x63\x64\x65\x66\x67\x71\x72\x73\x00\x10\x11\x22\x02\x00\x10\x91\xa9\x9a\x28\x02\xa1\x89\xa9\x9a\x88\x1a\x12\xa9\x9a\xaa\x89\x11\x62\x11\x8a\x99\x11\x13\x62\x66\x11\x11\xaa\x13\x62\x76\x58\x55\x49\x13\x20\x76\x56\x55\x38\x01\x20\x66\x67\x45\x34\x01\x20\x66\x67\x45\x38\x01\x00\x62\x66\x45\x14\x00\x00\x42\x66\x44\x14\x00\x00\x10\x11\x11\x01\x00"
         );
 
    
@@ -1115,7 +1171,7 @@ loop(void)
 
 #define TESTING
 #ifdef TESTING
-    static LED_minecraft1 *p = new LED_minecraft1();
+    static LED_galaga1 *p = new LED_galaga1();
     p->loop();
     led.display();
     started++;
