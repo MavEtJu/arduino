0105e0fbfcfceb61b98550bf2ba8a5dfbb972663
Edwin Groothuis <wireshark@mavetju.org>
2015-03-02 08:18:38 +1100

    implement proper millis()

--- pixelsim/Arduino.cpp
+++ pixelsim/Arduino.cpp
@@ -2,11 +2,11 @@
 #include <Arduino.h>
 #include <time.h>
 #include <Serial.h>
+#include <sys/time.h>
 
 void setup(void);
 void loop(void);
 class Serial Serial;
-time_t time_0 = time(NULL);
 
 const char *
 PSTR(const char *s)
@@ -35,5 +35,24 @@ delay(uint32_t ms)
 unsigned long
 millis(void)
 {
-	return (time(NULL) - time_0) * 1000;
+	struct timeval tv;
+	static long l0;
+	bool first = 1;
+
+	if (first) {
+		gettimeofday(&tv, NULL);
+		if (tv.tv_usec < 1000) {
+			tv.tv_usec += 1000000;
+			tv.tv_sec--;
+		}
+		tv.tv_usec -= 1000;
+		l0 = tv.tv_sec * 1000000 + tv.tv_usec;
+		l0 /= 1000;
+		first = 0;
+	}
+
+	gettimeofday(&tv, NULL);
+	long l = tv.tv_sec * 1000000 + tv.tv_usec;
+	l /= 1000;
+	return (l - l0);
 }
