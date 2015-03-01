5da5baca7b49c8aca9780ae135cdba36d5ad5abd
Edwin Groothuis <github@mavetju.org>
2015-03-01 22:07:25 +1100

    improve

--- toiletroll_draw/A_Tools.h
+++ toiletroll_draw/A_Tools.h
@@ -10,12 +10,12 @@ int freeRam(void);
 
 #define FREERAM(f) \
     Serial.print(f); \
-    Serial.print(F(": FreeRAM: ")); \
+    Serial.print(F(": FreeRam(): ")); \
     Serial.println(freeRam()); 
 
 #define FREEMEMORY(T) \
     Serial.print(T); \
-    Serial.print(F(": ")); \
+    Serial.print(F(": freeMemory(): ")); \
     Serial.println(freeMemory());
 
 #endif
