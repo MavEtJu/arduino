1bde935b7dd89f66f8b16dc6d88779b22429e578
Edwin Groothuis <wireshark@mavetju.org>
2015-03-07 21:37:21 +1100

    Remove stuff in other include files

--- ledsquare/animations.h
+++ ledsquare/animations.h
@@ -1,8 +1,7 @@
 #ifndef animations__h
 #define animations__h
 
-#define PARENTCONSTRUCTOR(__t__) __t__(LED_Strip *led, uint16_t VIEW_WIDTH, uint16_t VIEW_HEIGHT) : LED_Animation(led, VIEW_WIDTH, VIEW_HEIGHT) {}
-#define MYCONSTRUCTOR(__t__) __t__(LED_Strip *led, uint16_t VIEW_WIDTH, uint16_t VIEW_HEIGHT)
+#include "animation_squares.h"
 
 class LED_test : public LED_Animation {
     public:
@@ -43,34 +42,6 @@ class LED_lines1 : public LED_Animation {
     uint16_t x1_final, x2_final, y1_final, y2_final;
 
     uint16_t x1s[LED_lines1_history], x2s[LED_lines1_history], y1s[LED_lines1_history], y2s[LED_lines1_history];
-};
-
-class LED_squares1 : public LED_Animation {
-public:
-    MYCONSTRUCTOR(LED_squares1);
-    void animation(void);
-    LED c;
-};
-
-#define LED_squares2_history 20
-class LED_squares2 : public LED_Animation {
-public:
-    MYCONSTRUCTOR(LED_squares2);
-    void animation(void);
-    struct area a[LED_squares2_history];
-    LED c[LED_squares2_history], c_last;
-    void shift_history(struct area a, LED c);
-};
-
-#define LED_movingsquares1_squares 4
-class LED_movingsquares1 : public LED_Animation {
-public:
-    MYCONSTRUCTOR(LED_movingsquares1);
-    void animation(void);
-    LED c[LED_movingsquares1_squares];
-    int16_t x0[LED_movingsquares1_squares], y0[LED_movingsquares1_squares], x1[LED_movingsquares1_squares],
-        y1[LED_movingsquares1_squares], dx[LED_movingsquares1_squares], dy[LED_movingsquares1_squares],
-        size[LED_movingsquares1_squares], x[LED_movingsquares1_squares], y[LED_movingsquares1_squares];
 };
 
 class LED_cross1 : public LED_Animation {
