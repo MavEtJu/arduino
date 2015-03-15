a9060485d546e1889d22e5cbb96afdee78aac646
Edwin Groothuis <wireshark@mavetju.org>
2015-03-15 15:27:36 +1100

    Add hilbert1 curve

--- ledsquare/animations.h
+++ ledsquare/animations.h
@@ -53,4 +53,13 @@ public:
     LED *colourmap;
 };
 
+class LED_hilbert1 : public LED_Animation {
+public:
+    MYOWNCONSTRUCTOR(LED_hilbert1);
+    void animation(void);
+    int xy2d(int n, int x, int y);
+    void d2xy(int n, int d, int *x, int *y);
+    void rot(int n, int *x, int *y, int rx, int ry);
+};
+
 #endif
