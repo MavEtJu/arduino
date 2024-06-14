1c3d6bd3ab7429b18fb31ba9b3067aba61b5a6bc
Edwin Groothuis (on RPi4) <github@mavetju.org>
2024-06-14 11:01:20 +0100

    Adjust for arduino-mk.

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -20,10 +20,16 @@
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
+
+  Serial.begin(115200);
 }
+
+int i = 0;
 
 // the loop function runs over and over again forever
 void loop() {
+  Serial.print("Loop: ");
+  Serial.println(i++);
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
